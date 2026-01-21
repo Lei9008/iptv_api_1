8f9549670bb01af41578746639d3a51254480e16
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2026-01-21 15:16:54 +0800

    Update config1.py

--- config1.py
+++ config1.py
@@ -6,7 +6,7 @@ ip_version_priority = "ipv4"
 # 直播源URL列表
 source_urls = [
     
-    "https://gh.catmak.name/https://raw.githubusercontent.com/Lei9008/iptv_api_1/refs/heads/main/output/live_sources_ipv4.m3u",
+    "https://raw.githubusercontent.com/Lei9008/iptv_api_1/refs/heads/main/output/live_sources_ipv4.m3u",
        
  
 
