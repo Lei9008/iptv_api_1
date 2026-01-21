7d1cc8c0dc4ed1b7c1b306259e50d93f07b2f820
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2026-01-21 08:13:20 +0800

    Update speed_test.py

--- utils/speed_test.py
+++ utils/speed_test.py
@@ -148,7 +148,8 @@ class M3UProcessor:
 # 主程序
 async def main():
     # 输入输出文件路径
-    input_file = "input/live_sources.m3u"
+    #input_file = "input/live_sources.m3u"
+    input_file = "output/live_ipv4.m3u"
     output_file = f"{config.OUTPUT_DIR}/live_sources_sorted_{int(time.time())}.m3u"
     
     # 解析M3U文件
