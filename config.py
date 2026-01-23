64404e2c773cb15a0cb61d5fbb85fc004913f05c
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2026-01-23 09:36:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ source_urls = [
     "http://rihou.cc:555/gggg.nzk",
    # "https://github.com/plsy1/iptv/blob/main/multicast/multicast-jinan.m3u",
    # "https://github.com/liuchuang0321/K-TV/blob/master/output/user_result.txt",
-    "https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.m3u",
+   # "https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.m3u",
    
 
     
