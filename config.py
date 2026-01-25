c9e655f29c531982fc306fd9783c88004318cd3e
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2026-01-25 18:17:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ source_urls = [
    # "https://github.com/8080713/iptv-api666/blob/main/output/result.m3u",
    # "https://raw.githubusercontent.com/iptv-org/iptv/gh-pages/countries/cn.m3u",
    # "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u",
-    "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv4.m3u",
+   # "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv4.m3u",
    # "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/others_output.txt",
     #"https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv6.txt",
     #"https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt",
