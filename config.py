b8b4945c87413bcf28a613f897e008b2c54dbf5c
Lei9008 <133954672+Lei9008@users.noreply.github.com>
2026-01-25 09:13:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,18 @@
 
 # 直播源链接配置
 source_urls = [
-    "https://raw.kkgithub.com/develop202/migu_video/main/interface.txt",
+   
+    
     "https://github.com/develop202/migu_video/blob/main/interface.txt",
     "https://github.com/cyh92/iptv-api-weishi/blob/master/output/weishi.m3u",
     "https://github.com/cyh92/iptv-api-cctv/blob/master/output/cctv.m3u",
     "https://github.com/8080713/iptv-api666/blob/main/output/result.m3u",
-    "https://raw.githubusercontent.com/Guovin/iptv-database/master/result.txt",
     "https://raw.githubusercontent.com/iptv-org/iptv/gh-pages/countries/cn.m3u",
     "https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u",
     "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv4.m3u",
     "https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/others_output.txt",
     "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv6.txt",
     "https://raw.githubusercontent.com/vbskycn/iptv/master/tv/iptv4.txt",
-    "https://live.hacks.tools/tv/iptv6.txt",
     "https://live.hacks.tools/tv/iptv4.txt",
     "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv6.m3u",
     # 其他源链接...
